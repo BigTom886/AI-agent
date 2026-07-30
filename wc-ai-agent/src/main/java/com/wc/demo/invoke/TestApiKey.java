56e21f4efdbed3fa4cde7e0e3e771949cb56c4ee
BigTom886 <359388842@qq.com>
2026-07-30 23:41:13 +0800

    20260730
    1.初始化

---
+++ wc-ai-agent/src/main/java/com/wc/demo/invoke/TestApiKey.java
@@ -0,0 +1,8 @@
+package com.wc.demo.invoke;
+
+/*
+
+*/
+public interface TestApiKey {
+    String API_KEY="sk-922519c8fc75440d81c81d5fcb766dfe";
+}
---
+++ wc-ai-agent/src/test/java/com/wc/AppTest.java
@@ -0,0 +1,11 @@
+package com.wc;
+
+// import static org.junit.Assert.assertEquals;
+// import org.junit.Test;
+
+public class AppTest {
+    // @Test
+    // public void testAppOutput() {
+    //     assertEquals("Hello World!", "Hello World!");
+    // }
+}
